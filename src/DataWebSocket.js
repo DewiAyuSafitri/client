7455f61700bfbd5544f8107442886a8ba36d495b
Deasa092 <deasa092@gmail.com>
2025-01-22 16:38:18 +0700

    tampilan data

--- src/DataWebSocket.js
+++ src/DataWebSocket.js
@@ -1,18 +1,20 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function DataWebSocket() {
-
+  const [messages, setMessages] = useState([]);
   useEffect(() => {
     // Hubungkan ke server WebSocket menggunakan ws
-    const wsClient = new WebSocket("ws://localhost:8080");
+    const wsClient = new WebSocket("ws://localhost:8080/test");
 
     wsClient.onopen = () => {
       console.log("Connected to WebSocket Server");
     };
 
     wsClient.onmessage = (event) => {
+      setMessages((prevMessages) => [...prevMessages, event.data]);
       const dataServer = JSON.parse(event.data);
-          console.log("Data", dataServer)
+      console.log("Data", dataServer);
     };
 
     wsClient.onclose = () => {
@@ -25,7 +27,14 @@ function DataWebSocket() {
 
   return (
     <div style={{ fontFamily: "Arial, sans-serif", padding: "20px" }}>
-      <h1>Web Socket</h1>
+      <h1 className="title">Web Socket</h1>
+      <div className="messages-container">
+        {messages.map((message, index) => (
+          <p key={index} className="message-item">
+            {index + 1}. {message}
+          </p>
+        ))}
+      </div>
     </div>
   );
 }
